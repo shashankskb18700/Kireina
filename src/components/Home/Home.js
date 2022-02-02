46f1a77001ed27b1779b7ea483f2a70b589440a7
shashank <shashankskb18700@gmail.com>
2022-02-02 19:17:56 +0530

    fixed snapshot

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,19 +4,15 @@ import { dbService } from "../../firebase/fbase";
 import Search from "../search/search";
 
 const Home = () => {
-  const [item, setItem] = useState([]);
+  const [item, setItem] = useState({});
   const { onSnapshot, collection } = dbService;
 
   useEffect(() => {
-    // dbService.onSnapshot(dbService.collection(dbService.))
-    //need to do this thing for all four component
-    // onSnapshot(collection(dbService.getFirestore(), "header"), (snapshot) => {
-    //   const arr = snapshot.docs.map((docs) => ({
-    //     id: docs.id,
-    //     ...docs.data(),
-    //   }));
-    //   setItem(arr);
-    // });
+    const dataArr = ["headr", "month", "week", "allTime"];
+
+    const arc = [];
+    const newObj = {};
+
     const headerQuery = dbService.query(
       collection(dbService.getFirestore(), "header"),
       orderBy("id", "asc")
@@ -43,31 +39,25 @@ const Home = () => {
       week: weeklyQuery,
       allTime: allTimeQuery,
     };
-    const dataArr = ["headr", "month", "week", "allTime"];
 
-    console.log("again comming back let see item this time");
-    console.log(item);
-    const arc = [];
+    dataArr.map(async (a) => {
+      console.log(dataObj[a]);
 
-    dataArr.forEach((a) => {
-      console.log("now entered in it again");
-      console.log(item);
-      onSnapshot(dataObj[a], (snapshot) => {
+      await onSnapshot(dataObj[a], (snapshot) => {
+        console.log(snapshot.docs);
         const arr = snapshot.docs.map((docs) => ({
           id: docs.id,
           ...docs.data(),
         }));
-        // this a   in  set in not taking value of a
-        // let tempObj = new Object();
-        // tempObj[a] = arr;
-        // console.log("item fighters");
-        // console.log(item);
+        newObj[a] = arr;
         arc.push(arr);
         console.log(arc);
-        setItem(arc);
+        setItem(newObj);
       });
     });
 
+    console.log(dataObj);
+    console.log(newObj);
     // onSnapshot(headerQuery, (snapshot) => {
     //   const arr = snapshot.docs.map((docs) => ({
     //     id: docs.id,
@@ -92,3 +82,53 @@ const Home = () => {
 };
 
 export default Home;
+
+//
+
+//
+
+// for each runs completly before running of onSnapshot ,this might be happening because onSnapshot does not run instantly
+
+// snapshot is taking time and thats why all them runs and in the end only allTime is left and it keep on runntig for four time s
+
+// one  option is you can use four snapshot because you already  have four query
+
+//
+
+//
+
+// somehow this query thing is running for 4 time  in earlier and then other code run but why it should not  run whole thing form top to bottom .  and this thing causes issue  , when we are using newObj , and thats why when we reach snapshot a is only all time and we are keep on extracting that from  newObj
+
+// dbService.onSnapshot(dbService.collection(dbService.))
+//need to do this thing for all four component
+// onSnapshot(collection(dbService.getFirestore(), "header"), (snapshot) => {
+//   const arr = snapshot.docs.map((docs) => ({
+//     id: docs.id,
+//     ...docs.data(),
+//   }));
+//   setItem(arr);
+// });
+
+//________********)______________************)______________
+
+//_______**********__________************____________************____
+
+// const dataObj = {
+//   headr: headerQuery,
+//   month: monthlyQuery,
+//   week: weeklyQuery,
+//   allTime: allTimeQuery,
+// };
+
+// dataArr.forEach((a) => {
+//   newObj[a] = dbService.query(
+//     collection(dbService.getFirestore(), "allTime"),
+//     orderBy("id", "asc")
+//   );
+//   // console.log("newObj fans");
+//   // console.log(newObj);
+// });
+
+// when i sperated query i am getting all the  snapshot but when i take query with the help of for loop and am unable to get snapshot
+
+// so is this means something wrong with query  or    query is a different type of action and snapshot is a different type of action
