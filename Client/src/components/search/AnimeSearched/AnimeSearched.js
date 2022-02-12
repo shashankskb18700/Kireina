3f987f0fa18482e4e51deb9504ed12ec1e764a7b
shashank <shashankskb18700@gmail.com>
2022-02-12 20:37:46 +0530

    removed unnecessary assest

--- Client/src/components/search/AnimeSearched/AnimeSearched.js
+++ Client/src/components/search/AnimeSearched/AnimeSearched.js
@@ -2,30 +2,44 @@ import React from "react";
 import { connect } from "react-redux";
 
 const AnimeSearched = (props) => {
-  console.log(props.detail);
+  console.log(Object.keys(props.detail));
   const detail = Object.values(props.detail);
-  console.log(detail[0]);
-  const anime = Object.values(detail[0]);
-
-  console.log(anime);
   const arr = [];
-  const data = anime.map((d) => (
-    <div>
-      {console.log(d)}
-      {d.map((a) => (
-        <div>
-          {console.log(Object.keys(a.info[0])[1] === "img")}
+  const arrDetail = [];
+  if (Object.keys(props.detail).length !== 0) {
+    console.log(detail[0]);
+    const anime = Object.values(detail[0]);
 
-          {Object.keys(a.info[0])[1] === "img"
-            ? a.info[0].img.map((y) => (
-                <div>{arr.push(Object.values(y)[0].src)}</div>
-              ))
-            : ""}
-        </div>
-      ))}
-    </div>
-  ));
+    console.log(anime);
+
+    anime.map((d) => (
+      <div>
+        {console.log(d)}
+        {d.map((a) => (
+          <div>
+            {arrDetail.push(a)}
+            {console.log(Object.keys(a.info[0])[1] === "img")}
+            {console.log(Object.values(a.ratings[0])[0])}
+
+            {Object.keys(a.info[0])[1] === "img"
+              ? a.info[0].img.map((y) => (
+                  <div>
+                    {arr.push(
+                      Object.values(y)[0].height > 250
+                        ? Object.values(y)[0].src
+                        : ""
+                    )}
+                  </div>
+                ))
+              : ""}
+            <img src="#" />
+          </div>
+        ))}
+      </div>
+    ));
+  }
   console.log(arr);
+  console.log(arrDetail);
 
   return (
     <div>
