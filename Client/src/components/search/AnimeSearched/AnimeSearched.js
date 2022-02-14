3aa358c5fcc04af52d19669ce931762268407c75
shashank <shashankskb18700@gmail.com>
2022-02-14 18:51:23 +0530

    filterd api image data

--- Client/src/components/search/AnimeSearched/AnimeSearched.js
+++ Client/src/components/search/AnimeSearched/AnimeSearched.js
@@ -19,18 +19,10 @@ const AnimeSearched = (props) => {
           <div>
             {arrDetail.push(a)}
             {console.log(Object.keys(a.info[0])[1] === "img")}
-            {console.log(Object.values(a.ratings[0])[0])}
+            {/* {console.log(Object.values(a.ratings[0])[0])} */}
 
             {Object.keys(a.info[0])[1] === "img"
-              ? a.info[0].img.map((y) => (
-                  <div>
-                    {arr.push(
-                      Object.values(y)[0].height > 250
-                        ? Object.values(y)[0].src
-                        : ""
-                    )}
-                  </div>
-                ))
+              ? arr.push(Object.values(a.info[0].img[0])[0].src)
               : ""}
             <img src="#" />
           </div>
@@ -41,12 +33,17 @@ const AnimeSearched = (props) => {
   console.log(arr);
   console.log(arrDetail);
 
+  const fullDetail = (index) => {
+    console.log(index);
+    console.log(arrDetail[index]);
+  };
+
   return (
     <div>
       <h1>AnimeSearched</h1>
       {/* {data} */}
       {arr.map((a) => (
-        <img src={a} key={a} />
+        <img src={a} key={a} onClick={() => fullDetail(arr.indexOf(a))} />
       ))}
     </div>
   );
@@ -56,3 +53,15 @@ const mapStateToProps = (state) => {
   return { detail: state.srchRedu };
 };
 export default connect(mapStateToProps)(AnimeSearched);
+
+// arr.push(Object.values(a.info[0].img[0].src))
+
+// a.info[0].img.map((y) => (
+//   <div>
+//     {arr.push(
+//       Object.values(y)[0].height > 250
+//         ? Object.values(y)[0].src
+//         : ""
+//     )}
+//   </div>
+// ))
